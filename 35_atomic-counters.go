755211e1d7c894d6424573cebe0b0984c0fe633c
im-minion <vaibhavminiyar@gmail.com>
2020-09-12 21:46:31 +0530

    done 35_atomic-counters.go

--- 35_atomic-counters.go
+++ 35_atomic-counters.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops uint64
+	var wg sync.WaitGroup
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		//To atomically increment the counter we use AddUint64, giving it the memory address of our ops counter with the & syntax.
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&ops, 1)
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("ops:", ops)
+}
