7772d80f99aaff5a35e6329328a94756b9efff2f
im-minion <vaibhavminiyar@gmail.com>
2020-09-17 19:51:05 +0530

    done 66_environment-variables.go 67_http-clients.go 68_http-servers.go

---
+++ 68_http-servers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	// This handler does something a little more sophisticated by reading all the HTTP request headers
+	// and echoing them into the response body.
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", nil)
+}
