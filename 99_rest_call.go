6573dd8fed562d9d223f294fb652cf38cd6a9bb0
im-minion <vaibhavminiyar@gmail.com>
2020-11-09 22:40:58 +0530

    rest call GEt GetList Post

--- 99_rest_call.go
+++ 99_rest_call.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"bytes"
 )
 
 // Todo struct
@@ -18,6 +19,8 @@ type Todo struct {
 
 func main() {
 	get()
+	getList()
+	post()
 }
 
 func get() {
@@ -38,4 +41,46 @@ func get() {
 	var todoStruct Todo
 	json.Unmarshal(bodyBytes, &todoStruct)
 	fmt.Printf("API Response as struct %+v\n", todoStruct)
+}
+
+func getList() {
+	fmt.Println("2. Performing Http GetList...")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Convert response body to string
+	bodyString := string(bodyBytes)
+	fmt.Println("API Response as String:\n" + bodyString)
+
+	// Convert response body to Todo struct
+	var data []Todo
+	json.Unmarshal(bodyBytes, &data)
+	fmt.Printf("API Response as struct %+v\n", data)
+}
+
+func post() {
+    fmt.Println("3. Performing Http Post...")
+    todo := Todo{1, 2, "lorem ipsum dolor sit amet", true}
+    jsonReq, err := json.Marshal(todo)
+    resp, err := http.Post("https://jsonplaceholder.typicode.com/todos", "application/json; charset=utf-8", bytes.NewBuffer(jsonReq))
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    defer resp.Body.Close()
+    bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+    // Convert response body to string
+    bodyString := string(bodyBytes)
+    fmt.Println(bodyString)
+
+    // Convert response body to Todo struct
+    var todoStruct Todo
+    json.Unmarshal(bodyBytes, &todoStruct)
+    fmt.Printf("%+v\n", todoStruct)
 }
